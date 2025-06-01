09d6f9b65bf56ab7663748ca17b806ac8dcf610d
Brykl <rokirok430@gmail.com>
2025-06-01 22:05:25 +0500

    localhost ip

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,6 @@ import { testConnection } from "./config/dataBase";
 
 testConnection();
 
-app.listen(config.port, "172.30.0.66", () => {
-  console.log(`Server running on http://172.30.0.66:${config.port}`);
+app.listen(config.port, "localhost", () => {
+  console.log(`Server running on http://localhost:${config.port}`);
 });
